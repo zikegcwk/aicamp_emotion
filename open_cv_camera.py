8936aeca07a984baa3883685ccc9ec7bc665425f
Unknown <clement.h.ou@gmail.com>
2019-07-11 14:14:29 -0700

    comments & exception handling

--- open_cv_camera.py
+++ open_cv_camera.py
@@ -1,4 +1,4 @@
-import os  # added import time import numpy as np
+import os
 import sys
 import time
 
@@ -11,12 +11,20 @@ def main():
     assigned range :)
     """
 
-    emotion = sys.argv[1]
-    here = os.getcwd()  # added
+    try:  # set emotion
+        emotion = sys.argv[1]
+    except IndexError:
+        emotion = "happy"
+        print("No emotion specified, set to default \"happy\".")
+    try:  # set number of pics to take each time
+        numpics = sys.argv[2]
+    except IndexError:
+        numpics = 150
+        print("Number of images not specified, set to default 150.")
 
-    path = os.path.join(here, 'images', emotion)
+    path = os.path.join(os.getcwd(), 'images', emotion)
     if os.path.isdir(path):
-        print("Directory was not created since %s already exists" % path)
+        print("Directory was not created since %s already exists." % path)
     else:
         try:
             os.mkdir(path)
@@ -25,15 +33,15 @@ def main():
         else:
             print("Successfully created the directory %s " % path)
 
-    try:  # sets image counter to avoid overwriting old images
+    try:  # sets image counter to avoid overwriting old images from max of target directory
         img_counter = int(max(os.listdir(path)).split(".png")[0].split("_")[1]) + 1
-        print("Starting image counter at %s" % img_counter)
+        print("Starting image counter at %s." % img_counter)
     except ValueError:
         img_counter = 0
         print("No files detected, starting counter at 0")
 
     cam = cv2.VideoCapture(0)
-    print("Press space to take an image, esc or ctrl + c to exit. Currently taking %s pictures." % emotion)
+    print("Press space to take an image, esc or ctrl + c to exit.")
 
     cv2.namedWindow("OpenCVCamera")
 
@@ -47,12 +55,12 @@ def main():
             print("Escape hit, closing...")
             break
         if k % 256 == 32:  # space pressed
-            for x in range(0, 50):
+            for x in range(0, numpics):  # TODO add built-in user initiated break from this loop
                 ret, frame = cam.read()
                 cv2.imshow("test", frame)
                 cv2.waitKey(1)
                 img_name = "{}_{}.png".format(emotion, str(img_counter).zfill(4))
-                cv2.imwrite(os.path.join(here, 'images', emotion, img_name), frame)
+                cv2.imwrite(os.path.join(os.getcwd(), 'images', emotion, img_name), frame)
                 print("{} Saved".format(img_name))
                 img_counter += 1
                 time.sleep(.2)
