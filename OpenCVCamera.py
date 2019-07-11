fb0ffe2e68a055d23c8d8961b113b273bdb237a2
Unknown <clement.h.ou@gmail.com>
2019-07-11 13:23:40 -0700

    good version

--- OpenCVCamera.py
+++ OpenCVCamera.py
@@ -1,4 +1,5 @@
 import os  # added import time import numpy as np
+import sys
 import time
 
 import cv2
@@ -9,81 +10,53 @@ def main():
     --- You may adjust the amount of pictures you want for each press by changing the
     assigned range :)
     """
-    cam = cv2.VideoCapture(0)
 
-    cv2.namedWindow("test")
+    emotion = sys.argv[1]
     here = os.getcwd()  # added
 
-    img_counter = 0
-    print("To take an image Type h for Happy, s for Sad, p for Poker Face, and SpaceBar for Standard.(ESC to exit)")
+    path = os.path.join(here, 'images', emotion)
+    if os.path.isdir(path):
+        print("Directory was not created since %s already exists" % path)
+    else:
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("Creation of the directory %s failed" % path)
+        else:
+            print("Successfully created the directory %s " % path)
+
+    try:
+        img_counter = int(max(os.listdir(path)).split(".png")[0].split("_")[1]) + 1
+        print("Starting image counter at %s" % img_counter)
+    except ValueError:
+        img_counter = 0
+        print("No files detected, starting counter at 0")
+
+    cam = cv2.VideoCapture(0)
+    print("Press space to take an image, esc or ctrl + c to exit. Currently taking %s pictures." % emotion)
+
+    cv2.namedWindow("OpenCVCamera")
+
     while True:
         ret, frame = cam.read()
         cv2.imshow("test", frame)
         if not ret:
             break
         k = cv2.waitKey(1)
-
         if k % 256 == 27:
             # ESC pressed
             print("Escape hit, closing...")
             break
-        if k % 256 == 104:
-            for x in range(0, 100):
+        if k % 256 == 32:
+            for x in range(0, 50):
                 ret, frame = cam.read()
                 cv2.imshow("test", frame)
                 cv2.waitKey(1)
-                img_name = "HAPPY{}.png".format(img_counter)  # added
-                cv2.imwrite(os.path.join(here, 'images', 'happyface', img_name), frame)  # added
+                img_name = "{}_{}.png".format(emotion, str(img_counter).zfill(4))
+                cv2.imwrite(os.path.join(here, 'images', emotion, img_name), frame)
                 print("{} SAVED!".format(img_name))
                 img_counter += 1
                 time.sleep(.2)
-
-        if k % 256 == 115:
-            for x in range(0, 100):
-                ret, frame = cam.read()
-                cv2.imshow("test", frame)
-                cv2.waitKey(1)
-                img_name = "SAD{}.png".format(img_counter)  # added
-                cv2.imwrite(os.path.join(here, 'images', 'sadface', img_name), frame)  # added
-                print("{} SAVED!".format(img_name))
-                img_counter += 1
-                time.sleep(.3)
-
-        if k % 256 == 112:
-            for x in range(0, 100):
-                ret, frame = cam.read()
-                cv2.imshow("test", frame)
-                cv2.waitKey(1)
-                img_name = "POKER{}.png".format(img_counter)  # added
-                cv2.imwrite(os.path.join(here, 'images', 'pokerface', img_name), frame)  # added
-                print("{} SAVED!".format(img_name))
-                img_counter += 1
-                time.sleep(.3)
-
-        if k % 256 == 109:
-            for x in range(0, 100):
-                ret, frame = cam.read()
-                cv2.imshow("test", frame)
-                cv2.waitKey(1)
-                img_name = "MADFACE{}.png".format(img_counter)  # added
-                cv2.imwrite(os.path.join(here, 'images', 'madface', img_name), frame)  # added
-                print("{} SAVED!".format(img_name))
-                img_counter += 1
-                time.sleep(.3)
-
-        if k % 256 == 32:
-            for x in range(0, 100):
-                ret, frame = cam.read()
-                cv2.imshow("test", frame)
-                cv2.waitKey(1)
-                img_name = "StandardPicture{}.png".format(img_counter)  # added
-                cv2.imwrite(os.path.join(here, 'images', 'standard', img_name), frame)  # added
-                print("{} SAVED!".format(img_name))
-                img_counter += 1
-                time.sleep(.3)
-        # break#added
-
-    print("TO RUN AGAIN GO PRESS F5 IN CODING PAGE")  # added
     cam.release()
 
     cv2.destroyAllWindows()
