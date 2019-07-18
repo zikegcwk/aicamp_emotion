db6c47673c95082ad8dc387d0aedbfa4527f9a7b
Clement Ou <clement.h.ou@gmail.com>
2019-07-18 11:08:37 -0700

    make_path fix and style

--- bing_images_api.py
+++ bing_images_api.py
@@ -12,7 +12,7 @@ def download(url, file_name):
     url=url file_name=file location path
     """
     # open in binary mode- write file
-    with open(file_name, "wb") as file:
+    with open(file_name, 'wb') as file:
         # get request ask for the data from URL
         response = get(url)
         # write to file
@@ -25,14 +25,14 @@ def save_url(search_terms, image_count, term):
     search_terms=word being searched, image_count=# of images, term=type of image (happy, sad, etc)
     """
     # Bing Images API Information for URL *Insert Subscription Key!*
-    subscription_key = "a10ac1d7aff040f1b56a360ceb821b4f"
-    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    subscription_key = 'a10ac1d7aff040f1b56a360ceb821b4f'
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
     for val in search_terms:
         params = {
-            "q": val,
+            'q': val,
             # +define the term for offset
-            "offset": 0,
-            "imageType": "photo",
+            'offset': 0,
+            'imageType': 'photo',
             'minHeight': 100,
             'minWidth': 100,
             # +using count_step to determine # of URLs instead of range
@@ -43,24 +43,23 @@ def save_url(search_terms, image_count, term):
         offset_times = int((image_count / 150) + 1)
         next_offset = 0
         list_url = []
-        # Requests for ContentURL, loop to capture each URL
-        # res=response, search_results=list/dictionary of info, url=one captured URL, list_url=stored URLS from the search_terms
+        # Requests for ContentURL, loop to capture each URL res=response, search_results=list/dictionary of info,
+        # url=one captured URL, list_url=stored URLS from the search_terms
         for timer in range(offset_times):  # AND search_number is less than totalEstimatedMatches
             params.update({'offset': next_offset})
-            res = requests.get("https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=", headers=headers,
+            res = requests.get('https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=', headers=headers,
                                params=params)
             search_results = res.json()
             if image_count > search_results.get('totalEstimatedMatches'):
-                print("Error, too many images requested. Requested {} images, {} images exist".format(image_count,
-                                                                                                      search_results.get(
-                                                                                                          'totalEstimatedMatches')))
+                print('Error, too many images requested. Requested {} images, {} images exist'
+                      .format(image_count, search_results.get('totalEstimatedMatches')))
                 break
             else:
                 if search_results.get('value'):
                     for search_number in (search_results['value']):
                         url = search_number['contentUrl']
                         list_url.append(url)
-                        print("adding url:" + url)
+                        print('adding url:' + url)
                     next_offset = search_results.get('nextOffset')
                 # returns if there is no value in dictionary
                 else:
@@ -77,7 +76,7 @@ def save_url(search_terms, image_count, term):
                         prev_list_url = list(set(prev_list_url))
                     with open('output_file_{}'.format(term), 'wb') as fp:
                         pickle.dump(prev_list_url, fp)
-                except:
+                except:  # TODO add specific error code
                     with open('output_file_{}'.format(term), 'wb') as fp:
                         pickle.dump(list_url, fp)
 
@@ -95,16 +94,18 @@ def download_list(term, file_name, search_location):
     try:
         for idx, val in enumerate(download_urls, start=1):
             try:
-                download("{}".format(val), search_location + "/" + file_name + "{}".format(idx) + ".png")
-                print("currently downloading:{} out of {}".format(idx, total))
-            except:
+                download('{}'.format(val), search_location + '/' + file_name + '{}'.format(idx) + '.png')
+                print('currently downloading:{} out of {}'.format(idx, total))
+            except:  # TODO add specific error code
                 print(
-                    "Image downloaded is invalid or cannot be reached (internet), continuing with next image after a delay.")
+                    'Image downloaded is invalid or cannot be reached (internet), continuing with next image after a '
+                    'delay.')
                 time.sleep(10)
                 continue
-    except:
+    except:  # TODO add specific error code
         print(
-            "The folder location does not exist or the reference command failed. Please create a folder (case sensitive) or do a search.")
+            'The folder location does not exist or the reference command failed. Please create a folder (case '
+            'sensitive) or do a search.')
 
 
 def checklist(term):
@@ -132,19 +133,19 @@ if __name__ == '__main__':
 # (Use multiples of 150 for searches etc 150, 300, 450) input amount of images to search
 # image_count = 200
 # input TYPE of image searched (happy, sad, etc).
-# term = "happy"
+# term = 'happy'
 # save_url(search_terms, image_count, term)
 
 # To download the urls
 # input the TYPE of image searched
-# term = "happy"
+# term = 'happy'
 # input the name of the file (will name by file_name(1, 2, etc)
-# file_name = "happy people"
-# input where you want to save the file (for WINDOWS please add "r" BEFORE the search location, etc r"C:/Users/..."
-# search_location = r"C:\Banana\BingAPI\Test"
+# file_name = 'happy people'
+# input where you want to save the file (for WINDOWS please add 'r' BEFORE the search location, etc r'C:/Users/...'
+# search_location = r'C:\Banana\BingAPI\Test'
 # download_list(term, file_name, search_location)
 
 # To check how many images are downloaded
 # input the TYPE of image searched
-# term = "happy"
+# term = 'happy'
 # checklist(term)
--- make_path.py
+++ make_path.py
@@ -1,32 +1,32 @@
 import os
 import sys
 
+
 def make_path(folder_path, file_prefix):
-    '''split image files into train and validation dataset. 
+    """split image files into train and validation dataset.
         store their paths into txt files
-    '''
+    """
 
     file_list = os.listdir(folder_path)
 
-    for file1 in file_list:
-        print(folder_path, file1)
-    # make sure the file_list only contains file end with .jpg or .png
-
     # how to open a file in a write mode
-        train_f = open(file_prefix + '_train.txt', 'w')
-        test_f = open(file_prefix + '_test.txt', 'w')
+    train_f = open(file_prefix + '_train.txt', 'w')
+    test_f = open(file_prefix + '_test.txt', 'w')
 
-    # iterate through a list
-    
-        for idx , img in enumerate(file_list):
+    for file in file_list:
+        print(folder_path, file)
+        # make sure the file_list only contains file end with .jpg or .png
+
+        # iterate through a list
+
+        for idx, img in enumerate(file_list):
             img_path = os.path.join(folder_path, img)
 
             if idx % 9 == 0:
-             # write data into a file
-                 test_f.write(img_path + '\n')
+                # write data into a file
+                test_f.write(img_path + '\n')
             else:
-                 train_f.write(img_path + '\n')
-    
+                train_f.write(img_path + '\n')
 
     train_f.close()
     test_f.close()
@@ -34,8 +34,7 @@ def make_path(folder_path, file_prefix):
 
 if __name__ == '__main__':
     # inputs
-    args = sys.argv
-    # example: 
+    # example:
     # argv[1] = '/Users/mutishuman/Documents/images/happyface'
     # argv[2] = 'happy_bing_
-    make_path(argv[1], argv[2])
+    make_path(sys.argv[1], sys.argv[2])
