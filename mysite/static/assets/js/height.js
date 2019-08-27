51df23ecf97075a1ac1e22407f4e974efd6a7e15
Reduce395 <thomaschen395@gmail.com>
2019-08-26 19:13:34 -0700

    Deleting mysite (accidental files)

--- mysite/static/assets/js/height.js
+++
@@ -1,8 +0,0 @@
-$(document).ready ( function(){
-       var myImg = document.querySelector("#results");
-        var currWidth = myImg.clientWidth;
-        var currHeight = myImg.clientHeight;
-        var formHeight = (currWidth-300)
-		$('.parallax-container').css('height', formHeight);
-		});
-		
--- mysite/static/assets/js/init.js
+++
@@ -1,5 +0,0 @@
-  $(document).ready(function(){
-    $('.parallax').parallax();
-	$('.materialboxed').materialbox();
-  });
-  
