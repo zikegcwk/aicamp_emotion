51221161fc6cc0cbf09530d023abd39859b06303
Bernice Lau <bernicelau430@gmail.com>
2022-02-28 19:13:40 -0800

    removed and simplified link imports

--- bootstrap site/js/theme.js
+++ bootstrap site/js/theme.js
@@ -35,46 +35,5 @@
 
     });
 
- 
-    
-  // Smooth scrolling using jQuery easing
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-    
-    
-    $('a.js-scroll-trigger').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'ease');
-        event.preventDefault();
-    });
-
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').on('click', function(event) {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-   
-
-          /*START GOOGLE MAP*/
-          function initialize() {
-            var mapOptions = {
-              zoom: 15,
-              scrollwheel: false,
-              center: new google.maps.LatLng(40.7127837, -74.00594130000002)
-            };
-            var map = new google.maps.Map(document.getElementById('map'),
-                mapOptions);
-            var marker = new google.maps.Marker({
-              position: map.getCenter(),
-              icon: 'assets/img/map_pin.png',
-              map: map
-            });
-          }
-          google.maps.event.addDomListener(window, 'load', initialize);	
-          /*END GOOGLE MAP*/	
-
- 
 
 })(jQuery); // End of use strict
